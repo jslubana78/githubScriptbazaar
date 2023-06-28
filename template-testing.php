d2a3296610da3f4a5b97087ec6aed39dbb5a4e39
jslubana78 <jslubana78.js@gmail.com>
2023-06-28 20:36:06 +0530

    slider commit

--- template-testing.php
+++ template-testing.php
@@ -2,36 +2,62 @@
 <?php
 get_header();
 ?>
+
+
+
+<?php
+
+$args = array(
+'post_type' => 'post',
+);
+// The Query.
+$the_query = new WP_Query( $args );
+
+// The Loop.
+if ( $the_query->have_posts() ) {
+	?>
 <div class="postSlider">
 <section class="regular slider">
-    <div class="single-slide">
+     <?php
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		// echo '<li>' . esc_html( get_the_title() ) . '</li>';
+          $postId = get_the_ID();
+          $postFeatureImage = get_the_post_thumbnail_url($postId);
+          $postTitle = get_the_title($postId);
+          // $postExcerpt = get_the_excerpt($postId);
+          $postExcerpt = wp_trim_words( get_the_excerpt($postId), 22, '...' );
+          $postLink = get_the_permalink($postId);
+          ?>
+<div class="single-slide">
       <div class="main-slide">
-        <div class="image-div" style="background-image: url('http://placehold.it/350x300?text=1');">
+        <div class="image-div"  style="background-image: url(<?php echo $postFeatureImage; ?>);">
         </div>
         <div class="slide-text">
-          <h1>Heading Part</h1>
-          <p>excert excert excert excert excert excert excert excert excert excert excert excert</p>
-          <a href="#">Read More</a>
+          <h1><?php echo $postTitle; ?></h1>
+          <p><?php echo $postExcerpt; ?></p>
+          <a href="<?php echo $postLink; ?>">Read More</a>
         </div>
       </div>
     </div>
-    <div class="single-slide">
-      <img src="http://placehold.it/350x300?text=2">
-    </div>
-    <div class="single-slide">
-      <img src="http://placehold.it/350x300?text=3">
-    </div>
-    <div class="single-slide">
-      <img src="http://placehold.it/350x300?text=4">
-    </div>
-    <div class="single-slide">
-      <img src="http://placehold.it/350x300?text=5">
-    </div>
-    <div class="single-slide">
-      <img src="http://placehold.it/350x300?text=6">
-    </div>
+          <?php
+	}
+	// echo '</ul>';
+     ?>
   </section>
     </div>
+     <?php
+} else {
+	esc_html_e( 'Sorry, no posts matched your criteria.' );
+}
+// Restore original Post Data.
+wp_reset_postdata();
+
+
+?>
+
+    
+
 <?php
 get_footer();
 ?>
